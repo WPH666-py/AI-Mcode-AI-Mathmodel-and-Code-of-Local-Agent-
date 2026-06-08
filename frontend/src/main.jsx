b069713b89f62e547b74a7844d445897dfa929e7
WPH666-py <943050454@qq.com>
2026-06-08 14:41:52 +0800

    Upload via e-git

---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider, App as AntApp } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import 'katex/dist/katex.min.css'
+import App from './App'
+import './index.css'
+
+dayjs.locale('zh-cn')
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConfigProvider locale={zhCN} theme={{ token: { colorPrimary: '#1677ff' } }}>
+      <AntApp>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <App />
+        </BrowserRouter>
+      </AntApp>
+    </ConfigProvider>
+  </React.StrictMode>
+)
